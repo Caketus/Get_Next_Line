e00afa3d11884f692349835856e2ea9f0e28c69d
Caketus <52408823+Caketus@users.noreply.github.com>
2019-12-17 19:44:54 +0100

    Changed include from gnl_bonus.h to gnl.h

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line_bonus.h"
+#include "get_next_line.h"
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
